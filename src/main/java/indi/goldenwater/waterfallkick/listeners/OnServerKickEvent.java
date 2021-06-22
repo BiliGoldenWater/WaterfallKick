c688d743c36a2ea4182e847c7e5371bac252633e
BiliGoldenWater <2439577268@qq.com>
2021-06-22 17:28:43 +0800

    fix some NullPointerException error for unknown reason

--- src/main/java/indi/goldenwater/waterfallkick/listeners/OnServerKickEvent.java
+++ src/main/java/indi/goldenwater/waterfallkick/listeners/OnServerKickEvent.java
@@ -4,6 +4,7 @@ import indi.goldenwater.waterfallkick.WaterfallKick;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -11,17 +12,21 @@ import net.md_5.bungee.event.EventHandler;
 public class OnServerKickEvent implements Listener {
     @EventHandler
     public void onServerKickEvent(ServerKickEvent event) {
-        WaterfallKick plugin = WaterfallKick.getInstance();
+        final WaterfallKick plugin = WaterfallKick.getInstance();
         final ProxiedPlayer player = event.getPlayer();
 
-        String lobbyName = plugin.getConfig().getConfig().get("lobbyServerName");
-        String disabledServerName = plugin.getConfig().getConfig().get("disabledServer");
-        String playerServerName = player.getServer().getInfo().getName();
+        final String lobbyName = plugin.getConfig().getConfig().get("lobbyServerName");
+        final String disabledServerName = plugin.getConfig().getConfig().get("disabledServer");
+        final Server playerServer = player.getServer();
+        if (playerServer == null) return;
+        final ServerInfo playerServerInfo = playerServer.getInfo();
+        if (playerServerInfo == null) return;
+        final String playerServerName = playerServerInfo.getName();
 
         if (!(playerServerName.equals(lobbyName) ||
                 playerServerName.equals(disabledServerName) ||
                 event.getKickReasonComponent()[0].toPlainText().equals("[Proxy] Lost connection to server."))) {
-            ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo(lobbyName);
+            final ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo(lobbyName);
             event.setCancelled(true);
             event.setCancelServer(serverInfo);
             player.sendMessage(event.getKickReasonComponent());
